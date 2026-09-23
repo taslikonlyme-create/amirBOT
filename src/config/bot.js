cb3ee18e86950b1e90b8885bc92c58c583d72dc8
taslikonlyme-create <taslikonlyme@gmail.com>
2026-09-23 23:01:52 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        name: "AmirKING network┃System bot", // required by Discord API, not shown in the client
+        state: "playing Minecraft",     // this is what people actually see
+        type: 5,               // Custom
       },
     ],
   },
